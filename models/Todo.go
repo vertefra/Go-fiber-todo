a16d235ecab328385bb900fc53056ab7fe052560
vertefra <verte.fra@gmail.com>
2020-10-13 09:49:36 -0400

    controllers

--- models/Todo.go
+++ models/Todo.go
@@ -12,3 +12,14 @@ type Todo struct {
 	Description      string `json:"description" bson:"description"`
 	Done             bool   `json:"done" bson:"done"`
 }
+
+// CreateTodo is a wrapper function that creates a newTodo Object.
+// It returns a Todo pointer to a Todo struct that contains all
+// all the specified data
+func CreateTodo(title, description string) *Todo {
+	return &Todo{
+		Title:       title,
+		Description: description,
+		Done:        false,
+	}
+}
