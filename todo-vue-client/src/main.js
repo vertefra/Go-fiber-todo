4627e895bf60d8af144a35c6b702d7f3df0830e3
Philip Gray <pcgrayiii@icloud.com>
2020-10-15 16:04:55 -0700

    Missed a bug

--- todo-vue-client/src/main.js
+++ todo-vue-client/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import LoginPage from './components/LoginPage.vue'
+
 
 const app = createApp(App)
 
-app.component('login-page', LoginPage)
+
 
 app.mount('#app')
